4effe44f6f3d71d89946b6b53b9b7969e0da22b9
Rafael Fernandes <rafael_fernandescf@hotmail.com>
2021-11-12 10:06:19 -0300

    config terraform

--- 01-terraform_lab_exemplos/05-resource/main.tf
+++ 01-terraform_lab_exemplos/05-resource/main.tf
@@ -1,87 +1,36 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
 
-# resource "aws_instance" "web" {
-#   ami           = "ami-09e67e426f25ce0d7"
-#   instance_type = "t2.micro"
-#   subnet_id              = "subnet-eddcdzz4"
-#   # availability_zones = ["us-east-1"] # configurando zona para criar maquinas
-#   tags = {
-#     Name = "Minha Maquina Simples EC2"
-#   }
-#   volume_id = aws_ebs_volume.itau_volume_encrypted.id
-# }
-
-
-# resource "aws_ebs_volume" "itau_volume_encrypted" {
-#   size      = 8
-#   encrypted = true
-#   tags      = {
-#     Name = "Volume itaú"
-#   }
-# }
-
-
-
-##### Caso Itaú #####
-# aws configure # maquina pessoal
-# nas do itaú colocar as variáveis de ambiente no bashrc
-# https://docs.aws.amazon.com/sdkref/latest/guide/environment-variables.html
-# configurar via environment variable
-# export AWS_ACCESS_KEY_ID=""
-# export AWS_SECRET_ACCESS_KEY=""
-# export AWS_DEFAULT_REGION=""
-
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# resource "aws_instance" "web" {
-#   ami = "ami-09e67e426f25ce0d7"
-#   instance_type = "t3.micro"
-#   subnet_id = "subnet-05880ea9006199004"
-  
-#   tags = {
-#     Name = "MinhaPrimeiraMaquina-Carol  "
-#   }
-# }
-
-# resource "aws_ebs_volume" "itau_volume_encrypted" {
-#   size      = 8
-#   encrypted = true
-#   availability_zone = "us-east-1a"
-#   tags      = {
-#     Name = "Volume itaú"
-#   }
-# }
-
-# resource "aws_volume_attachment" "ebs_att" {
-#   device_name = "/dev/sdh"
-#   volume_id   = aws_ebs_volume.itau_volume_encrypted.id
-#   instance_id = aws_instance.web.id
-# }
-
-# Gamaacademythree-sbx - # passago a chave pelo pessoal de segurança itaú
-# export AWS_ACCESS_KEY_ID="3232323232"
-# export AWS_SECRET_ACCESS_KEY="34433444sssdd3+ssa/dd434343"
-
-# //////
-
+# criando máquina na AWS de forma automatica
 # ///////// do fernando zerati //////
 provider "aws" {
-  region = "sa-east-1"
+  region = "us-east-2"
 }
 resource "aws_instance" "web" {
-  subnet_id     = "subnet-05d2f48a5e97f0b1a"
-  ami= "ami-054a31f1b3bf90920"
+  count = 1
+  subnet_id     = "subnet-061ee529ff557a85d"
+  ami= "ami-0629230e074c580f2"
   instance_type = "t2.micro"
+  associate_public_ip_address = true  
   root_block_device {
     encrypted = true
     volume_size = 8
   }
   tags = {
-    Name = "ec2-zerati-tf"
+    Name = "ec2-fernandes-tf-${(count.index+1)}"
   }
 }
-# /////
+
+
+
+
+
+
+
+# https://www.terraform.io/docs/language/values/outputs.html
+output "instance_public_dns" {
+  value = [
+      aws_instance.web[0].public_ip, 
+      aws_instance.web[0].private_ip,
+      "ssh -i C:/Users/rafae/.ssh/chave_privada2.pem ubuntu@${aws_instance.web.public_ip}"
+  ]
+  description = "Mostra o DNS e os IPs publicos e privados da maquina criada."
+}
--- 01-terraform_lab_exemplos/11-resource_security_group/main.tf
+++ 01-terraform_lab_exemplos/11-resource_security_group/main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "ami-0629230e074c580f2"
   instance_type = "t2.micro"
   key_name = "treinamento-turma1_itau" # Nome da Key gerada pelo ssk-keygem e upada na AWS
   tags = {
     Name = "Minha Maquina Simples EC2"
   }
+  
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
 }
